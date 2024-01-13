dfcb7fbb7d2c84bf989031a5e3bdd115782cc82c
harry pattani <harrypattani15@gmail.com>
2024-01-13 19:26:38 +0530

    till section 3

--- game.js
+++ game.js
@@ -1,14 +1,25 @@
 var buttonColours =     ["red" , "blue", "green " , "yellow"];
+let gamePattern = new Array();
 
 
 
 function nextSequence(){
    var blah = Math.floor(Math.random()*4);
    
-   var randomChosenColour = buttonColours[blah];
+   var randomChosenColour  = buttonColours[blah];
 
 
-let gamePattern = new Array();
+
 gamePattern.push (randomChosenColour);
 
+$ ("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+var myAudio = new Audio("sounds/"+ randomChosenColour + ".mp3");
+myAudio.play();
+
+
 };
+
+
+
+
