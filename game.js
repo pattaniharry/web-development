6d9e96c3bf8b6c59f7609bf8cb7fbff557ae79e5
harry pattani <harrypattani15@gmail.com>
2024-01-13 15:48:40 +0530

    created a new pattern

---
+++ game.js
@@ -0,0 +1,14 @@
+var buttonColours =     ["red" , "blue", "green " , "yellow"];
+
+
+
+function nextSequence(){
+   var blah = Math.floor(Math.random()*4);
+   
+   var randomChosenColour = buttonColours[blah];
+
+
+let gamePattern = new Array();
+gamePattern.push (randomChosenColour);
+
+};
