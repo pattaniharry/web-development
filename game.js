5987c751135be9935b2e3455edfe1569dca08d72
harry pattani <harrypattani15@gmail.com>
2024-01-15 10:11:53 +0530

    full function game till game over

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ let userClickedPattern = new Array();
 
 
 
-var buttonColours =     ["red" , "blue", "green " , "yellow"];
+var buttonColours =     ["red" , "blue", "green" , "yellow"];
 let gamePattern = new Array();
 
 
@@ -18,35 +18,30 @@ $(document).keydown(function(event){
    }
    });
       
-
+   $("div[type=button] ").click(function(){
+      var userChosenColour = $(this).attr('id');
+      playSound(userChosenColour);
+      userClickedPattern.push (userChosenColour);
+      console.log(userClickedPattern);
+      animatePress(userChosenColour);
+      checkAnswer(userClickedPattern.length-1);
+   });
 
 
 function nextSequence(){
    
    var blah = Math.floor(Math.random()*4);
-   
    var randomChosenColour  = buttonColours[blah];
 
-
-
 gamePattern.push (randomChosenColour);
-
 $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-
 playSound(randomChosenColour);
 
 
-$("div[type=button] ").click(function(){
-   var userChosenColour = $(this).attr('id');
-   playSound(userChosenColour);
-   userClickedPattern.push (userChosenColour);
-   console.log(userClickedPattern);
-   animatePress(userChosenColour);
-});
 
  
 $("#level-title").text("level " + level);
+
 level++;
 
 
@@ -67,11 +62,32 @@ setTimeout(function()  {
    var ToAnimate = $('#' +currentColour);
    ToAnimate.removeClass("pressed");
 }, 100);
-
-
-
-
-
 };
 
+
+function checkAnswer(currentlevel){
+
+   if(gamePattern.length-1 == userClickedPattern.length-1){
+
+      if(gamePattern.toString() == userClickedPattern.toString()){
+        
+
+         setTimeout(nextSequence,1000);
+         userClickedPattern.length = 0;
+      
+   } else{
+      var myAudio = new Audio("sounds/wrong.mp3");
+      myAudio.play();
+
+      $("body").addClass("game-over")
+
+      setTimeout(function() {
+         $("body").removeClass("game-over")
+      }, 200);
+      
+      $("h1").text("Game over Press Any Key to Restart");
+   };
+  
+};
+};
 
