bc41e5d3f347d6fbe285be64fb0f86cb452eea6d
harry pattani <harrypattani15@gmail.com>
2024-01-25 19:54:36 +0530

    given + password checker in js

---
+++ SecretsProject/index.js
@@ -0,0 +1,31 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import bodyParser from "body-parser";
+import x from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var userIsAuthorized = false;
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+function PasswordChecker(req,res,next){
+  const password = req.body["password"];
+  if(password === 'ILoveProgramming'){
+    userIsAuthorized = true;
+  }
+  next(); 
+}
+
+app.use(PasswordChecker)
+
+app.get("/", (req ,res) => {
+    res.sendFile(__dirname + "/public/index.html")
+  });
